56f4932a95b86ddad4f4d600ec90061554e7139a
Daniel Mulroy <dan@danmulroy.com>
2023-03-12 17:24:02 +0100

    fix(.env): checks for existence of .env file, adds feedback and error handling

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,31 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const { getGptSummary } = require('./utils');
-const data = fs.readFileSync('.env', 'utf8');
-const envVars = data.split('\n');
-envVars.forEach(envVar => {
-  const ENV_FROM_FILE = {};
-  if (envVar.includes('=')) {
-    const [key, value] = envVar.split('=');
-    ENV_FROM_FILE[key] = value;
-  }
-  process.env = { ENV_FROM_FILE, ...process.env };
-});
+const path = require('path');
+const { getGptSummary } = require('./index');
+
+try {
+  const file = path.resolve(process.cwd(), '.env');
+  //file exists
+  if (fs.existsSync(file)) {
+    const data = fs.readFileSync(file, 'utf8');
+    const envVars = data.split('\n');
+    envVars.forEach(envVar => {
+      const ENV_FROM_FILE = {};
+      if (envVar.includes('=')) {
+        const [key, value] = envVar.split('=');
+        ENV_FROM_FILE[key] = value;
+      }
+      process.env = { ENV_FROM_FILE, ...process.env };
+    });
+  } else
+    console.log(
+      'Did not find .env file at',
+      file,
+      '\n if this is unexpected, make sure you are running this script from the same directory as your .env file.'
+    );
+} catch (err) {
+  console.error('Error reading vars from .env:', err);
+}
 
 // Parse command line arguments
 const args = {};
